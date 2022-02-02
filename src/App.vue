cef9a83f892e5812e8eb684677d9eb0dfc80ad20
Julien LARDANT <julien.lardant@actived.fr>
2022-02-02 14:36:27 +0100

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,93 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>{{title}}</h1>
+
+<!--  Example Modal simple: envoie des datas par props et event (call fonction d'un component) pour fermeture-->
+  <p>Example de modal simple avec props et event</p>
+  <div v-if="showModal">
+    <Modal :header="header" :textModal="textModal" theme="sale" @close="toggleModal()" ></Modal>
+  </div>
+  <button @click="toggleModal()">
+    Open Modal simple
+  </button>
+
+
+<!--  Example Modal avec SLOT: Permet d'envoyé un html différent dans la modal pour la rendre plsu rétulisable ( props aussi si on veut)-->
+  <p>Show Modal With slot</p>
+  <div v-if="showModalSlot">
+    <ModalSlot :headerSlot="headerSlot" @close="toggleModalSlot()" >
+<!--      titre send by props et html par slot -->
+        <h1>{{ headerSlot }}</h1>
+        <p>Et voila un html envoyé par slot au Component</p>
+    </ModalSlot>
+  </div>
+  <button @click="toggleModalSlot()">
+    Open Modal With Slot
+  </button>
+
+  <!-- Example de teleport: use to  + css selector to say where telport the component -->
+  <p>Example de modal simple teleport ( dans le body , cf index.html)</p>
+  <teleport to=".modalsTeleport" v-if="showModalTeleport">
+    <Modal :header="headerTeleport" :textModal="textModal" @close="toggleModalTeleport" ></Modal>
+  </teleport>
+  <button @click="toggleModalTeleport">
+    Open Modal Teleport
+  </button>
+
+
 </template>
 
+
+
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Modal from './components/Modal.vue'
+import ModalSlot from './components/ModalSlot.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Modal , ModalSlot
+  },
+  data() {
+    return {
+      title : 'New feature Vue3',
+      header : 'Modal simple example',
+      headerSlot : 'Titre send by props',
+      headerTeleport : 'Modal teleporté dans un autre elt du dom',
+      textModal:'text send by props',
+      showModal : false,
+      showModalSlot : false,
+      showModalTeleport : false
+    }
+  },
+  methods : {
+    toggleModal () {
+      console.log()
+      this.showModal = !this.showModal
+    },
+    toggleModalSlot () {
+      this.showModalSlot = !this.showModalSlot
+    },
+    toggleModalTeleport () {
+      this.showModalTeleport = !this.showModalTeleport
+    }
+
   }
 }
 </script>
 
 <style>
-#app {
+#app, .modalsTeleport {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+h1 , {
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
 }
 </style>
